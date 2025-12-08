ed47c1952a59d1201dda27d8fc9efc952b1f1c90
bandarzheuskaya <cougar.anasta.06@gmail.com>
2025-12-08 17:46:17 +0300

    фикс

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import os
+
 
 
 def setup_page():
@@ -11,5 +13,9 @@ def setup_page():
 
 
 def load_styles():
-    with open("styles.css", "r", encoding="utf-8") as f:
+    base = os.path.dirname(__file__)
+    css_path = os.path.join(base, "styles.css")
+
+    with open(css_path, "r", encoding="utf-8") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
